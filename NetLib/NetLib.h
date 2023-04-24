2ebb2861ba5d5e20cf48e0fcd7d6025cfd846b62
Aleksey Molokovich <aleksey@ya.ru>
2023-04-24 22:04:20 +0300

    Initial Commit

---
+++ NetLib/NetLib.h
@@ -0,0 +1,18 @@
+//
+//  NetLib.h
+//  NetLib
+//
+//  Created by Алексей Молокович on 24.04.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetLib.
+FOUNDATION_EXPORT double NetLibVersionNumber;
+
+//! Project version string for NetLib.
+FOUNDATION_EXPORT const unsigned char NetLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetLib/PublicHeader.h>
+
+
